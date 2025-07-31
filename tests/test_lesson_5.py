1f0bc07eee756e55c2b40c064a64e68455cbdb71
Alex <kend1123ue@gmail.com>
2025-07-31 12:52:11 -0500

    chore: dagster init

---
+++ tests/test_lesson_5.py
@@ -0,0 +1,24 @@
+import dagster as dg
+
+import dagster_essentials.completed.lesson_5.defs
+from dagster_essentials.completed.lesson_5.defs.assets import metrics, trips
+
+
+def test_assets():
+    assets = [
+        trips.taxi_trips_file,
+        trips.taxi_zones_file,
+        trips.taxi_trips,
+        trips.taxi_zones,
+        metrics.trips_by_week,
+        metrics.manhattan_stats,
+        metrics.manhattan_map,
+    ]
+    result = dg.materialize(assets)
+    assert result.success
+
+
+def test_defs():
+    assert dg.Definitions.merge(
+        dg.components.load_defs(dagster_essentials.completed.lesson_5.defs)
+    )
---
+++ tests/test_lesson_6.py
@@ -0,0 +1,30 @@
+import dagster as dg
+
+import dagster_essentials.completed.lesson_6.defs
+from dagster_essentials.completed.lesson_6.defs.assets import metrics, trips
+from dagster_essentials.completed.lesson_6.defs.resources import database_resource
+
+
+def test_assets():
+    assets = [
+        trips.taxi_trips_file,
+        trips.taxi_zones_file,
+        trips.taxi_trips,
+        trips.taxi_zones,
+        metrics.trips_by_week,
+        metrics.manhattan_stats,
+        metrics.manhattan_map,
+    ]
+    result = dg.materialize(
+        assets=assets,
+        resources={
+            "database": database_resource,
+        },
+    )
+    assert result.success
+
+
+def test_defs():
+    assert dg.Definitions.merge(
+        dg.components.load_defs(dagster_essentials.completed.lesson_6.defs)
+    )
