da58f1b1966cfb85530f06df51c99d5b1a46d07b
Alex <kend1123ue@gmail.com>
2025-07-31 17:08:37 -0500

    feat: add duck_db asset

--- src/dagster_essentials/defs/assets/trips.py
+++ src/dagster_essentials/defs/assets/trips.py
@@ -1,7 +1,12 @@
 import requests
 from dagster_essentials.defs.assets import constants
+import dagster as dg
+import duckdb
+import os
+from dagster._utils.backoff import backoff
 
 
+@dg.asset
 def taxi_trips_file() -> None:
     """
     The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
@@ -15,3 +20,49 @@ def taxi_trips_file() -> None:
         constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
     ) as output_file:
         output_file.write(raw_trips.content)
+
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+    This asset will contain a unique identifier and name for each part of NYC as a distinct taxi zone.
+    """
+    raw_taxi_zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
+
+
+@dg.asset(deps=["taxi_trips_file"])
+def taxi_trips() -> None:
+    """
+    The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
